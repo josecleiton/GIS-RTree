9c87369a29f8038f323076222f4160077a5683b5
josecleiton <jcleitonbc@gmail.com>
2018-08-25 08:01:24 -0300

    QT Project

---
+++ backend.cpp
@@ -0,0 +1,3 @@
+#include "backend.h"
+using namespace std;
+
---
+++ backend.h
@@ -0,0 +1,10 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <numeric>
+
+#endif // BACKEND_H
