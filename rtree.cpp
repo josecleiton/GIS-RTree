006f4640132f7c7227551cc88bd564954b702f6b
Icaro Leite <icaaro.leite@gmail.com>
2018-10-03 09:05:14 -0300

    rtree

--- rtree.cpp
+++ rtree.cpp
@@ -1,1 +1,9 @@
 #include "rtree.hpp"
+using namespace SpatialIndex;
+
+Node::Node(){
+}
+
+RTree::RTree(){
+
+}
--- rtree.hpp
+++ rtree.hpp
@@ -9,29 +9,38 @@ namespace SpatialIndex{
  * Não faço ideia de como implementar essa classe
  */
 
-class RTree{
 
-    enum{
-        ORDEM = 4,
-        MINNODES = ORDEM/2,
-        MAXNODES = ORDEM,
-    };
-
-    struct Node;
-
-    struct Branch{
-        Retangulo MBR;
-        union{
-            Node* ChildPointer;
-            size_t Dado; // Guarda o indice da forma em disco
-        };
-    };
-
-    struct Node{
-        int m_Nivel;
-        int m_Count;
-        vector<Branch> m_Branch;
-    };
+enum{
+    ORDEM = 4,
+    MINNODES = ORDEM/2,
+    MAXNODES = ORDEM,
 };
+
+struct Node;
+
+struct Chave{
+    Retangulo MBR;
+    union{
+        Node* ChildPointer;
+        size_t Dado; // Guarda o indice da forma em disco
+    };
+    Chave();
+};
+
+struct Node{
+    int m_Nivel;
+    int m_Count;
+    vector<Chave> mchaves;
+    Node();
+};
+
+class RTree{
+private:
+    Node* raiz;
+
+public:
+   RTree();
+};
+
 }
 #endif // RTREE_HPP
